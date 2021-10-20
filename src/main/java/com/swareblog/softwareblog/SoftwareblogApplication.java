e512c6fc998934200c1642eca71955d272bf9fb7
Mrwu123 <39589471+Mrwu123@users.noreply.github.com>
2021-10-20 16:13:59 +0100

    init project

---
+++ src/main/java/com/swareblog/softwareblog/SoftwareblogApplication.java
@@ -0,0 +1,13 @@
+package com.swareblog.softwareblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareblogApplication.class, args);
+    }
+
+}
